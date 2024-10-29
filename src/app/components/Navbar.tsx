16214b18dcf9888272bfa37d4b4507b3c089909f
asadkhan25 <asadkhan.ak.786.25@gmail.com>
2024-10-29 20:04:21 +0500

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import "../globals.css";
+
+import Link from "next/link"
+export default function Navbar ()
+{
+    
+    return(
+       
+        <div id="navbar">
+        <h3 className="logo">amazon.in</h3>
+       <div id="subnav">
+            <a className="ab" href="#">HOME</a>
+            <a className="ab"href="#">MY CART</a>
+            <a className="ab"href="#">CONTACT US</a>
+            <a className="ab"href="#">RETURN </a>
+            <input placeholder="Search amazon.in" />
+            <button>Search...</button>
+        </div>
+    </div>
+    )
+}
